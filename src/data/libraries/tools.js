5aae91f2f7f15779c48a78b25a8098f45ae379c0
Christian Opitz <christian.opitz@netresearch.nr>
2017-04-10 12:36:41 +0200

    Added semann

--- src/data/libraries/tools.js
+++ src/data/libraries/tools.js
@@ -2,6 +2,16 @@ module.exports = {
     title: 'Tools',
     items: [
         {
+            title: 'Semann - semantic annotator',
+            glyphicon: 'indent-right',
+            desc: 'Enhance any content with annotations from various sources (e.g. DBpedia). The project provides a JavaScript SDK which shows a widget with relevant entities for the text being analyzed which can be selected for inclusion on the target content in various ways (e.g. RDFa attributes, inclusion to sidebars, footers, injected paragraphs etc.). The backends that analyze the content are invoked using adapters (currently Apache Stanbol, others will follow). The SDK is designed to allow annotations on any web sites / apps which will involve some programming effort to get it running - however there are standard integrations for common editors and systems (currently CKEditor, more to come).',
+            badges: {
+                demo: 'https://netresearch.github.io/semann',
+                semann: 'github',
+                'semann-adapters': 'github'
+            }
+        },
+        {
             title: 'AssetPicker',
             glyphicon: 'hand-up',
             desc: 'AssetPicker is a free asset or file picker designed to be easily included into web application interfaces. It has a file abstraction layer allowing adapters to connect to any remote storage, be it cloud storages like Amazon S3, Google Drive or Dropbox or assets from a custom web application server. In opposite to other file managers or pickers, AssetPicker is suitable for hierarchical as well as associative file storages.',
